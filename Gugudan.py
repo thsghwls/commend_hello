d8553a69ce81c52b2a9b4e84c526520f0893dd09
sonhojin <thsghwls301@naver.com>
2017-04-19 18:09:09 +0900

    CREATE / 2단 출력 동작 by hojin

---
+++ Gugudan.py
@@ -0,0 +1,14 @@
+print("## 구구단 출력 프로그램 ##")
+
+dan   = 2
+count = 1
+
+print(dan) 
+print("단")
+while count < 10:
+    print(dan)
+    print(" * ") 
+    print(count) 
+    print(" = ")
+    print(count * dan)
+    count += 1
--- hello.py
+++
@@ -1,9 +0,0 @@
-print("파이썬 기초.Ver1.0.0")
-a = 4
-b = 8
-a + b
-c = "Hojin"
-
-while a > 0 :
-    print(c)
-    a = a-1
